2b11aa68f17cdf48a3c01cac77ecb2fcfc9a1491
marin <46572320+katelyynn@users.noreply.github.com>
2026-07-06 17:29:18 +0100

    feat: npm

---
+++ index.js
@@ -0,0 +1,13 @@
+import album_track from './album_track.json' assert { type: 'json' };
+import artist from './artist.json' assert { type: 'json' };
+import combined_artists from './combined_artists.json' assert { type: 'json' };
+import redirects from './redirects.json' assert { type: 'json' };
+import romanised_artists from './romanised_artists.json' assert { type: 'json' };
+
+export {
+    album_track,
+    artist,
+    combined_artists,
+    redirects,
+    romanised_artists
+}
