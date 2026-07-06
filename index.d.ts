afbf5c3b1b1d86dfddaecfdc10a279cced2a60b8
marin <46572320+katelyynn@users.noreply.github.com>
2026-07-06 17:37:33 +0100

    chore: guh

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
-export { };
-declare module '@tealmiku/lotus';
+export declare const album_track: any;
+export declare const artist: any;
+export declare const combined_artists: any;
+export declare const redirects: any;
+export declare const romanised_artists: any;
